3794804901e08ce65e2ef4dfd7aa904b152d5149
Akshat Gupta <gakshat0694@gmail.com>
2024-06-23 16:09:43 +0530

    added delete and checkmark function

--- todo.js
+++ todo.js
@@ -1,3 +1,4 @@
+let index = 1;
 // function to load todos
 function loadTodo() {
     const todo = JSON.parse(localStorage.getItem("todos")) || { "todolist": [] };
@@ -14,8 +15,29 @@ function addTodoToLocalStorage(todoText) {
 function appendTodoInHTML(todoText){
     const list = document.getElementById("ul"); //fetch ul
     const todo = document.createElement("li"); // create li tag
-    todo.textContent = todoText; // add text in li
+    todo.setAttribute("id", {index});
+
+    todo.innerHTML = `${todoText}<button id ="del ${index}" class="del">Delete</button>` ; // add text in li
+    
     list.appendChild(todo); // add li as a child in list
+}
+function Delete(delList){
+    // const delList = document.getElementById(id);
+    if(delList.innerHTML==="Delete"){
+    delList.parentNode.remove();
+}
+}
+function checkmark(checkList){
+    // if(checkList.style.backgroundColor === "#eafae2"){
+    //     console.log(checkList.style.backgroundColor)
+    //     checkList.style.backgroundColor = "white"
+
+    // }
+    // else{
+        
+        checkList.style.backgroundColor = "#eafae2";
+        
+    // }
 }
 // an event listner which is activated when dom content is loaded (async)
 document.addEventListener("DOMContentLoaded", () => {
@@ -23,6 +45,19 @@ document.addEventListener("DOMContentLoaded", () => {
     const todoInput = document.getElementById("input"); //return input element
     const todoSubmit = document.getElementById("submit");// return submit button element
     const list = document.getElementById("ul");
+    //when to delete
+    list.addEventListener("click",(e)=>{
+        const tar = e.target;
+        // console.log(tar);
+        Delete(tar);
+    })
+    //to checkmark as done
+    list.addEventListener("click",(e)=>{
+        const tar =e.target;
+        console.log(tar);
+        checkmark(tar);
+    })
+    
 // when submit button is clicked this event is to be executed
     todoSubmit.addEventListener("click", () => {
         const todoText = todoInput.value; // return the text entered by the user
@@ -30,10 +65,25 @@ document.addEventListener("DOMContentLoaded", () => {
             alert("Write something") // displays alert if submit is clicked without any text
         }
         else {
-            addTodoToLocalStorage(todoText); //adds the text to the local storage
+            // addTodoToLocalStorage(todoText); //adds the text to the local storage
             appendTodoInHTML(todoText); //append text to html
             todoInput.value = ""; // make value null after appending
         }
+    })
+    //When pressed enter
+    todoInput.addEventListener("keypress", (e) => {
+        if(e.key == "Enter") 
+        {
+            const todoText = todoInput.value; // return the text entered by the user
+        if (todoText == "") {
+            alert("Write something") // displays alert if submit is clicked without any text
+        }
+        else {
+            // addTodoToLocalStorage(todoText); //adds the text to the local storage
+            appendTodoInHTML(todoText); //append text to html
+            todoInput.value = ""; // make value null after appending
+        }
+    }
     })
 // when mouse clicked out of focus (after writing) event activates
     todoInput.addEventListener("change", (event) => {
